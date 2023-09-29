8143e8af5ede0ac918949d29e9d383b6de4a7060
SzczurekYT <kamilsecret8@gmail.com>
2023-09-29 22:19:27 +0200

    Init

---
+++ src/main/java/yt/szczurek/robotsystem/RobotSystem.java
@@ -0,0 +1,17 @@
+package yt.szczurek.robotsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RobotSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
